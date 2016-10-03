0ee21cd8c3ae16e63e0f290d93a55f7c65e8b5af
tylerklose <tylerklose@gmail.com>
2016-10-03 13:14:58 -0400

    added 10 questions to each AddEPA component and started to style index.html

--- med360/app/components/AddEPA.js
+++ med360/app/components/AddEPA.js
@@ -5,23 +5,80 @@ var AddEPA = React.createClass({
 	handleClick: function(e) {
 		var obj = {
 			name: document.getElementById('name').value,
-			description: document.getElementById('description').value
+			description: document.getElementById('description').value,
+			questions: []
 		}
+		questions = document.getElementsByClassName('enter-question');
+		for (var i = 0; i < questions.length; i++) {
+			obj.questions.push(questions.item(i).value);
+		}
+
 		console.log(JSON.stringify(obj));
+		console.log(document.getElementsByClassName('enter-question'));
 	},
 
 	render: function() {
 		return(
-			<div>
+			<div id="epa-container">
 				<h3>Add a new EPA</h3>
 				<div>
-					<label htmlFor="name">Name</label><br />
-					<input type="textbox" id="name"/>
+					<label htmlFor="name">Name: </label>
+					<input type="textbox" id="name" />
+				</div>
+
+				<div id="description-div">
+					<label htmlFor="description">Description: </label>
+					<textarea id="description" />
 				</div>
 
 				<div>
-					<label htmlFor="description">Description</label><br />
-					<textarea id="description" />
+					Question 1: 
+					<input className="enter-question" type="textbox" id="question-1" />
+				</div>
+
+				<div>
+					Question 2: 
+					<input className="enter-question" type="textbox" id="question-2" />
+				</div>
+
+				<div>
+					Question 3: 
+					<input className="enter-question" type="textbox" id="question-3" />
+				</div>
+
+				<div>
+					Question 4: 
+					<input className="enter-question" type="textbox" id="question-4" />
+				</div>
+
+				<div>
+					Question 5: 
+					<input className="enter-question" type="textbox" id="question-5" />
+				</div>
+
+				<div>
+					Question 6: 
+					<input className="enter-question" type="textbox" id="question-6" />
+				</div>
+
+				<div>
+					Question 7: 
+					<input className="enter-question" type="textbox" id="question-7" />
+				</div>
+
+				<div>
+					Question 8: 
+					<input className="enter-question" type="textbox" id="question-8" />
+				</div>
+
+				<div>
+					Question 9: 
+					<input className="enter-question" type="textbox" id="question-9" />
+				</div>
+
+				<div>
+					Question 10: 
+					<input className="enter-question" type="textbox" id="question-10" />
 				</div>
 
 				<div>
