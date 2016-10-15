ede0739f8fc3bff15fd93791e7d885a42dc6ec89
Marquis Hackett <marqhack@dhcp-v880-5-00089.1Xwireless.unc.edu>
2016-10-15 15:54:22 -0400

    fixed up file structure

--- code/webapp.js
+++ code/webapp.js
@@ -4,63 +4,34 @@ var db = require('./dbhandler');
 var app = express();
 var api = express();
 
-//var epajson = {'epaNum': 1, 'name':'Professionalism', 'description': 'How Professional the med student is.'};
-//db.addEPA(epajson);
 
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
 
-
-var bugData = [ 
-    {'title':'brokenthing', 'id':1, 'owner':'me', 'status':'my bad', 'priority':'ffffuuu'},
-    {'title':'secondbrokenthing', 'id':2, 'owner':'him', 'status':'also my bad', 'priority':'ffffuuu'},
-];
-
-api.get('/medFeedback', function(req, res){
+api.use(bodyParser.json( { type: '*/*' })); 
+api.get('/epalist', function(req, res){
     var query = db.getDB();
     spillEPAs();
 
     function spillEPAs() {
-      query.all("SELECT * FROM EPAs", function(err, rows){
-         res.json(JSON.stringify(rows));
-    });
-}
-
+        query.all("SELECT * FROM EPAs", function(err, rows){
+            res.json(JSON.stringify(rows));
+        });
+    }
 });
 
-api.post('/medFeedback', function(req, res){
+api.post('/epalist', function(req, res){
     var stmt = db.prepare("UPDATE medFeedback INSERT INTO students VALUES(?, ?)");
     stmt.run(row.pid + 1, "name");
     stmt.finalize();
 }); 
 
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-});
 
-//App root
-app.get('/', function (req, res) {
-    res.send("Just some root, move along, nothing to see here.");
-});
 
 //API root
-api.use(bodyParser.json( { type: '*/*' })); 
-api.get('/', function(req, res){
-    res.send("This is the API! Congrats");
-});
-api.get('/bugs', function (req, res) {
-    res.send(JSON.stringify(bugData));
-});
 
-api.post('/bugs', function (req, res) {
-    
-
-    console.log('start length: '+ bugData.length);
-    var newBug = req.body;
-    newBug.id = bugData.length + 1;
-    bugData.push(newBug); 
-    res.send('POST request to homepage');
-    console.log('end length: '+ bugData.length);
-});
 
 app.use('/api', api)
 app.use(express.static('static'));
