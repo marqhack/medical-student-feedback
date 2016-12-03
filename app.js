860c3ad50cf9dca1c693f14f54cdf3a363e6eddc
marqhack <marqhack@gmail.com>
2016-12-03 16:07:55 -0500

    moved app.js and www to root of project

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var routes = require('./epa-tracker/routes/index');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, './epa-tracker/views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
@@ -19,7 +19,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, './epa-tracker/public')));
 
 app.use('/', routes);
 
